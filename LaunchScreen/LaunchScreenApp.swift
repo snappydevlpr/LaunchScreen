cc26fa1cf329a625e02296578b2ab846640499f5
Austin Suarez <austinmsuarez@gmail.com>
2022-05-09 20:41:43 -0500

    Initial Commit

---
+++ LaunchScreen/LaunchScreenApp.swift
@@ -0,0 +1,17 @@
+//
+//  LaunchScreenApp.swift
+//  LaunchScreen
+//
+//  Created by Austin Suarez on 5/9/22.
+//
+
+import SwiftUI
+
+@main
+struct LaunchScreenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
